44d0dc9bf411d3817c023fd4d632f934171306d4
rodrigograca31 <rodrigograca1993@gmail.com>
2020-01-21 19:10:54 +0000

    Endpoints

--- cars/router.js
+++ cars/router.js
@@ -17,4 +17,44 @@ router.get("/", (req, res) => {
 		});
 });
 
+router.post("/", (req, res) => {
+	Cars.insert(req.body)
+		.then(result => {
+			if (result) {
+				res.json(result);
+			} else {
+				res.json({});
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: "error" });
+		});
+});
+router.put("/:id", (req, res) => {
+	Cars.update({ id: req.params.id, ...req.body })
+		.then(result => {
+			if (result) {
+				res.json(result);
+			} else {
+				res.json({});
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: "err.message" });
+		});
+});
+router.delete("/:id", (req, res) => {
+	Cars.deleteEntry(req.params)
+		.then(result => {
+			if (result) {
+				res.json(result);
+			} else {
+				res.json({});
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: "err.message" });
+		});
+});
+
 module.exports = router;
--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,7 @@ module.exports = {
 		},
 		seeds: {
 			directory: "./data/seeds"
-		}
+		},
+		useNullAsDefault: true
 	}
 };
