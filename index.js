97815581b322a76ec873757bffd65c42b85ad3ce
rodrigograca31 <rodrigograca1993@gmail.com>
2020-01-21 14:38:30 +0000

    🎉 Initial Setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const carsRouter = require("./cars/router.js");
+
+const server = express();
+
+server.use(express.json());
+
+// console.log(carsRouter);
+
+server.use("/api/cars", carsRouter);
+
+server.use("/api/aa", (req, res) => {
+	console.log("hey works!");
+	res.json({ message: "works" });
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log("running on " + port));
